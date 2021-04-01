73786e6a7f6de5a8cdd1ec3127b9f8ec5c496a09
Michael Fatemi <myfatemi04@gmail.com>
2021-04-01 13:50:05 -0400

    Add STL file format

--- src/stl.rs
+++ src/stl.rs
@@ -18,6 +18,10 @@ impl Vector {
 			z: self.z / magnitude
 		};
 	}
+
+	pub fn to_ascii(&self) -> String {
+		format!("{} {} {}", self.x, self.y, self.z)
+	}
 }
 
 pub struct Triangle {
@@ -59,5 +63,48 @@ impl Triangle {
 
 	pub fn normal(&self) -> Vector {
 		return self.normal;
+	}
+
+	pub fn to_ascii(&self) -> String {
+		let mut result = String::new();
+		result.push_str(format!("facet normal {}\n", self.normal.to_ascii()));
+		result.push_str("\touter loop\n");
+		
+		for val in &[self.a, self.b, self.c] {
+			result.push_str(format!("\t\tvertex {}\n", val.to_ascii()));
+		}
+
+		result.push_str("\tendloop\n");
+		result.push_str("endfacet\n");
+
+		result
+	}
+}
+
+pub struct STL {
+	name: String,
+	triangles: Vec<Triangle>
+}
+
+impl STL {
+	pub fn init(name: String) -> STL {
+		STL {
+			name,
+			triangles: Vec::new()
+		}
+	}
+
+	// For the full format for an STL file, look here:
+	// https://en.wikipedia.org/wiki/STL_(file_format)
+	pub fn to_ascii(&self) -> String {
+		let mut result = String::new();
+		result.push_str(format!("solid {}", self.name));
+
+		for triangle in &self.triangles {
+			result.push_str(format!("{}\n", &triangle.to_ascii()));
+		}
+
+		result.push_str(format!("endsolid {}", self.name));
+		result
 	}
 }
